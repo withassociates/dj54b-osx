bd13aa545b2d8268a6dc7808209ad217b31c07af
Jamie White <jamie@jgwhite.co.uk>
2015-02-14 18:11:18 +0000

    Initial Commit

---
+++ DJ54BTests/DJ54BTests.swift
@@ -0,0 +1,36 @@
+//
+//  DJ54BTests.swift
+//  DJ54BTests
+//
+//  Created by Jamie White on 14/02/2015.
+//  Copyright (c) 2015 Jamie White. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class DJ54BTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
